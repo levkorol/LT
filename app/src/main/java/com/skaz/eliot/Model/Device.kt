3111d95e9a311ff9f13b5c79bc2f8fe446068ecd
lev <sasiskajik@mail.ru>
2020-05-25 20:47:49 +0300

    c

--- app/src/main/java/com/skaz/eliot/Model/Device.kt
+++ app/src/main/java/com/skaz/eliot/Model/Device.kt
@@ -9,7 +9,7 @@ data class Device (
     val category: String
 //    val deviceInfo: DeviceInfo?,
 //    val last_data: DeviceAllData?,
-//    val deviceTariff: DeviceTariff?,
+//    val deviceTariff: DeviceTariff?, 
 //    val interval: Boolean,
 //    val startDateToShow: String,
 //    val endDateToShow: String
