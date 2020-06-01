6466920059a1dd7eeb9a90c058fbbfe7c6f1f85f
Slobodyaniuk <rabotinsky@gmail.com>
2020-06-02 01:16:18 +0300

    Рефакторинг запроса устройств

--- app/src/main/java/com/skaz/eliot/Services/DataService.kt
+++ app/src/main/java/com/skaz/eliot/Services/DataService.kt
@@ -14,11 +14,24 @@ import kotlin.collections.ArrayList
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import com.skaz.eliot.Model.*
+import java.lang.reflect.Type
 
 object DataService {
 
     val devices = ArrayList<Device>()
     val dec = DecimalFormat("#.##")
+
+    fun electricIndicationsRequest(request: ElectricIndicationsRequest, onResponse: (ElectricIndicationsResponse?) -> Unit) {
+        makeJsonObjectRequest<ElectricIndicationsRequest, ElectricIndicationsResponse>(URL_ELECTRIC_GET_INDICATIONS, request, onResponse);
+    }
+
+    fun waterIndicationsRequest(request: WaterIndicationsRequest, onResponse: (WaterIndicationsResponse?) -> Unit) {
+        makeJsonObjectRequest<WaterIndicationsRequest, WaterIndicationsResponse>(URL_WATER_GET_INDICATIONS, request, onResponse)
+    }
+
+    fun deviceRequest(request: DevicesRequest, onResponse: (List<Device>?) -> Unit) {
+        makeJsonArrayRequest(URL_DEVICES, request, object: TypeToken<List<Device>>() {}.type, onResponse)
+    }
 
     private fun <TRequest, TResponse> makeJsonObjectRequest(url: String, request: TRequest, onResponse: (TResponse?) -> Unit) {
         val gson = GsonBuilder().create()
@@ -55,13 +68,12 @@ object DataService {
         App.prefs.requestQueue.add(req)
     }
 
-    private fun <TRequest, TResponse> makeJsonArrayRequest(url: String, request: TRequest, onResponse: (List<TResponse>?) -> Unit) {
+    private fun <TRequest, TResponse> makeJsonArrayRequest(url: String, request: TRequest, responseType: Type, onResponse: (List<TResponse>?) -> Unit) {
         val gson = GsonBuilder().create()
         val requestBody = gson.toJson(request)
         val req = object :
-            JsonObjectRequest(Method.POST, url, null, Response.Listener { response ->
+            JsonArrayRequest(Method.POST, url, null, Response.Listener { response ->
                 try {
-                    val responseType = object : TypeToken<List<TResponse>>() {}.type
                     val response : List<TResponse> = gson.fromJson(response.toString(), responseType)
                     onResponse(response)
                 } catch (e: Exception) {
@@ -89,56 +101,5 @@ object DataService {
             }
         }
         App.prefs.requestQueue.add(req)
-    }
-
-    fun electricIndicationsRequest(request: ElectricIndicationsRequest, onResponse: (ElectricIndicationsResponse?) -> Unit) {
-        makeJsonObjectRequest<ElectricIndicationsRequest, ElectricIndicationsResponse>(URL_ELECTRIC_GET_INDICATIONS, request, onResponse);
-    }
-
-    fun waterIndicationsRequest(request: WaterIndicationsRequest, onResponse: (WaterIndicationsResponse?) -> Unit) {
-        makeJsonObjectRequest<WaterIndicationsRequest, WaterIndicationsResponse>(URL_WATER_GET_INDICATIONS, request, onResponse)
-    }
-
-    fun deviceRequest(request: DevicesRequest, onResponse: (List<Device>?) -> Unit) {
-        val gson = GsonBuilder().create()
-        val requestBody = gson.toJson(request)
-
-        val devicesRequest = object :
-            JsonArrayRequest(Method.POST, URL_DEVICES, null, Response.Listener { response ->
-                try {
-                    this.devices.clear()
-                    val listType = object : TypeToken<List<Device>>() {}.type
-                    val devs : List<Device>  = gson.fromJson(response.toString(), listType)
-                    this.devices.addAll(devs)
-
-                    onResponse(devs)
-
-                } catch (e: JSONException) {
-
-                    onResponse(null)
-                }
-
-            }, Response.ErrorListener { error ->
-                Log.d("ERROR", "Could not login user: $error")
-                onResponse(null)
-            }) {
-
-            override fun getBodyContentType(): String {
-                return "application/json"
-            }
-
-            override fun getBody(): ByteArray {
-                return requestBody.toByteArray()
-            }
-
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String, String>()
-                headers.put("Accept", "application/json; version=1")
-                headers.put("X-Platform", "Android")
-                return headers
-            }
-        }
-
-        App.prefs.requestQueue.add(devicesRequest)
     }
 }
